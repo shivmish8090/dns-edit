3a013ab59a4f72b0cd3ff2d45d27a39731eafa5a
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-03-03 09:41:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 Rajputshivsingh65. All rights reserved.
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+# Written by Rajputshivsingh65 <choudharydilip256@gmail.com>, January 2025.
+
+import os
+DB_NAME = os.getenv("DB_NAME", "edit")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+MONGO_URI = os.getenv("MONGO_URL")
+OWNER_ID = os.getenv("OWNER_ID")
+LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID"))
